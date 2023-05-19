ae9e0b4aba5ce675192f6634eb614c6c09cfb124
Luis Zendel Samperio <serverless.19@gmail.com>
2023-05-19 13:13:37 -0600

    feat: add post functionality

--- app/controller/event.js
+++ app/controller/event.js
@@ -1,11 +1,10 @@
-const {  postDataSensor, gatDataEvent } = require("../../config/mysql")
+const { gatDataEvent, postDataEvent } = require("../../config/mysql");
 
-const getData =  ({req, res}) => {
-    gatDataEvent({req,res})
-}
-const postData = (req,res) => {
-    postDataSensor({req,res})
-}
+const getData = ({ req, res }) => {
+  gatDataEvent({ req, res });
+};
+const postData = (req, res) => {
+  postDataEvent({ req, res });
+};
 
-
-module.exports = {getData, postData}
+module.exports = { getData, postData };
--- config/mysql.js
+++ config/mysql.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-const  con = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: "robot",
   password: "1234",
   database: "uaeh",
 });
 
-const gatDataEvent = ({req,res}) => {
+const gatDataEvent = ({ req, res }) => {
   con.query("select * from evento", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
@@ -14,14 +14,21 @@ const gatDataEvent = ({req,res}) => {
   });
 };
 
-const postDataSensor = ({req,res}) => {
-    console.log(req)  
-  /*const querySql = "insert into datasensor (valueSensing) values ("+ data.value +");";
+const postDataEvent = ({ req, res }) => {
+  console.log(req.body);
+  var postData = req.body
+  const nombre = postData.nombre
+  const fechaI = postData.fechaInicio
+  const fechaF = postData.fechaFin
+  const ob = postData.observaciones
+  const querySql = `insert into Evento (nombre,fechaInicio,fechaTermino,observaciones)
+  values ('${nombre}', '${fechaI}', '${fechaF}', '${ob}')`
+  console.log(querySql)
   con.query(querySql, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
-    res.json(result);
-  });*/
-}
+    res.status(201).json(result);
+  });
+};
 
-module.exports = {gatDataEvent, postDataSensor}
+module.exports = { gatDataEvent, postDataEvent };
